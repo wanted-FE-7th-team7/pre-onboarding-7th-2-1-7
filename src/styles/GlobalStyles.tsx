bddbd46fbd08c11505d5de7716a65c54f994989e
Chaedie <chaedong.im.dev@gmail.com>
2022-11-03 00:12:05 +0900

    Style: 글로벌 스타일 지정 (styled-reset)
    공통으로 사용할 theme, flex등 상수화 하여 theme.ts 파일에 저장

---
+++ src/styles/GlobalStyles.tsx
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyles = createGlobalStyle`${reset}
+  html {
+    font-size:10px;
+  }
+`;
+
+export default GlobalStyles;
---
+++ src/styles/theme.ts
@@ -0,0 +1,22 @@
+export const theme = {
+  black: '#000000',
+  gray: '#D9D9D9',
+  blue: '#0094FF',
+};
+
+export const flex = (
+  justifyContent = 'center',
+  alignItems = 'center',
+  flexDirection = 'row'
+) => {
+  return `
+  display: flex;
+  justify-content: ${justifyContent};
+  align-items: ${alignItems};
+  flex-direction: ${flexDirection};`;
+};
+
+export const width = {
+  sm: '360px',
+  lg: '450px',
+};
