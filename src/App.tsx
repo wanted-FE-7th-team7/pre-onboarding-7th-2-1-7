4b17fcd12e5a7dbc2e8c75c31128ab75f36ca99a
Leeseul-Moon <leeseulmoon4@gmail.com>
2022-11-02 23:50:26 +0900

    Feat: 라우터 및 전역 스타일링 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HeaderBar from './components/UI/HeaderBar';
+import DetailPage from './pages/DetailPage';
+import ListPage from './pages/ListPage';
+import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
-  return <div className="App">여기에 앱을 작성해주세요.</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <HeaderBar />
+      <Router>
+        <Routes>
+          <Route path="/" element={<ListPage />} />
+          <Route path="/carList/:id" element={<DetailPage />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
