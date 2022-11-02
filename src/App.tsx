222f1f6662386cc60b89cd5a4b18bcf457c7cff5
so0112 <tkddh0112@gmail.com>
2022-11-02 22:19:06 +0900

    Feat: Route 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
 import { ThemeProvider } from 'styled-components';
-import ItemTag from './components/UI/ItemTag';
+import All from './components/All';
 import GlobalStyles from './styles/GlobalStyles';
 import { theme } from './styles/theme';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ListPage from './pages/ListPage';
+import DetailPage from './pages/DetailPage';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <ItemTag />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<All />} />
+          <Route path="/list" element={<ListPage />} />
+          <Route path="/detail" element={<DetailPage />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
