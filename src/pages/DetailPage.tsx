391f15e9068509a6c656045130520b5fb2e2f84d
Chaedie <chaedong.im.dev@gmail.com>
2022-11-03 03:17:38 +0900

    Feat: 디테일페이지 데이터 표현

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,32 +1,46 @@
 import { useLocation } from 'react-router';
 import styled from 'styled-components';
+import DetailName from '../components/DetailName';
 import HeaderBar from '../UIs/HeaderBar';
+import Image from '../UIs/Image';
 import ListHeader from '../UIs/ListHeader';
 import ListItem from '../UIs/ListItem';
+import dayjs from 'dayjs';
 
 interface Props {}
+interface Insuarance {
+  name: string;
+  description: string;
+}
+interface AdditionalProducts {
+  name: string;
+  amount: number;
+}
 
 function DetailPage() {
   const location = useLocation();
-  const { id, startDate, insurance, additionalProducts } = location.state;
+  const { id, startDate, insurance, additionalProducts, amount } =
+    location.state;
   const { brand, name, segment, fuelType, imageUrl } = location.state.attribute;
 
   return (
     <Wrapper>
       <HeaderBar title="차량상세" />
+      <Image src={imageUrl} size="DETAIL_PAGE" />
+      <DetailName brand={brand} name={name} amount={amount} />
 
-      <div>
-        <img src={imageUrl} alt="car" />
-      </div>
-      <ListHeader title="월 비용" />
-      <ListItem name="차종" title="{segment}" />
-      <ListItem name="차종" title="{segment}" />
-      <ListItem name="차종" title="{segment}" />
-      <ListHeader title="월 비용" />
-      <ListItem name="차종" title="{segment}" />
-      <ListItem name="차종" title="{segment}" />
-      <ListHeader title="월 비용" />
-      <ListItem name="차종" title="{segment}" />
+      <ListHeader title="차량 정보" />
+      <ListItem name="차종" title={segment} />
+      <ListItem name="연료" title={fuelType} />
+      <ListItem name="이용 가능일" title={startDate.toLocaleString()} />
+      <ListHeader title="보험" />
+      {insurance.map(({ name, description }: Insuarance) => (
+        <ListItem key={name} name={name} title={description} />
+      ))}
+      <ListHeader title="추가 상품" />
+      {additionalProducts.map(({ name, amount }: AdditionalProducts) => (
+        <ListItem key={name} name={name} title={amount.toLocaleString()} />
+      ))}
     </Wrapper>
   );
 }
