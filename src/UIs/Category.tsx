b76881f3ff61e4d97b06c862378f6e0b8d12c683
Chaedie <chaedong.im.dev@gmail.com>
2022-11-03 00:23:40 +0900

    Style: 리스트 페이지 UI 배치

---
+++ src/UIs/Category.tsx
@@ -0,0 +1,7 @@
+import { PropsWithChildren } from 'react';
+
+function Category({ children }: PropsWithChildren) {
+  return <div>{children}</div>;
+}
+
+export default Category;
--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -4,6 +4,10 @@ import Error from '../components/Error';
 import Loading from '../components/Loading';
 import useFetch from '../hooks/useFetch';
 import { Cars } from '../interfaces/Cars';
+import Category from '../UIs/Category';
+import HeaderBar from '../UIs/HeaderBar';
+import Tag from '../UIs/Tag';
+
 function ListPage() {
   const [cars, setCars] = useState<Cars[]>([]);
   const [isLoading, errors] = useFetch(setCars);
@@ -14,6 +18,15 @@ function ListPage() {
 
   return (
     <>
+      <HeaderBar title="전체차량" />
+      <Category>
+        <Tag isSelected={true} placeholder="전체" />
+        <Tag isSelected={false} placeholder="대형" />
+        <Tag isSelected={false} placeholder="중형" />
+        <Tag isSelected={false} placeholder="소형" />
+        <Tag isSelected={false} placeholder="전기" />
+      </Category>
+
       {isLoading ? <Loading /> : <CarList cars={cars} />}
     </>
   );
