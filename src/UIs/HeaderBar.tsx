f3b7bffc36102d3cbe52731ba9ec367e3818ff71
Chaedie <chaedong.im.dev@gmail.com>
2022-11-03 03:16:37 +0900

    Feat: 헤더바 컴포넌트, 아이콘백 구현

--- src/UIs/HeaderBar.tsx
+++ src/UIs/HeaderBar.tsx
@@ -1,21 +1,45 @@
+import { useNavigate } from 'react-router';
 import styled from 'styled-components';
 import { flex, theme, width } from '../styles/theme';
+import IconBack from './IconBack';
 
 interface Props {
   title: string;
 }
 
 function HeaderBar({ title }: Props) {
-  return <StyledHeaderBar>{title}</StyledHeaderBar>;
+  const navigate = useNavigate();
+  return (
+    <StyledHeaderBar>
+      {title === '차량상세' ? (
+        <div className="ICON_back" onClick={() => navigate(-1)}>
+          <IconBack />
+        </div>
+      ) : (
+        <div />
+      )}
+      {title}
+      <div />
+    </StyledHeaderBar>
+  );
 }
 
 export default HeaderBar;
 
 const StyledHeaderBar = styled.div`
-  ${flex()}
+  ${flex('space-between')}
   border-bottom: 1px solid ${theme.black};
   font-weight: 700;
   font-size: 1.7rem;
   height: 6rem;
   width: ${width.lg};
+  padding: 0 2rem;
+
+  div {
+    width: 2.4rem;
+    height: 2.4rem;
+  }
+  .ICON_back {
+    cursor: pointer;
+  }
 `;
