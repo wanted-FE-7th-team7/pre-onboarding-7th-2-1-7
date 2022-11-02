240e6d41142c9e249ee2571277cb47671813f4d1
Chaedie <chaedong.im.dev@gmail.com>
2022-11-03 01:54:33 +0900

    Feat: 디테일 페이지 구현

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,7 +1,36 @@
+import { useLocation } from 'react-router';
+import styled from 'styled-components';
+import HeaderBar from '../UIs/HeaderBar';
+import ListHeader from '../UIs/ListHeader';
+import ListItem from '../UIs/ListItem';
+
 interface Props {}
 
 function DetailPage() {
-  return <h1>DetailPage</h1>;
+  const location = useLocation();
+  const { id, startDate, insurance, additionalProducts } = location.state;
+  const { brand, name, segment, fuelType, imageUrl } = location.state.attribute;
+
+  return (
+    <Wrapper>
+      <HeaderBar title="차량상세" />
+
+      <div>
+        <img src={imageUrl} alt="car" />
+      </div>
+      <ListHeader title="월 비용" />
+      <ListItem name="차종" title="{segment}" />
+      <ListItem name="차종" title="{segment}" />
+      <ListItem name="차종" title="{segment}" />
+      <ListHeader title="월 비용" />
+      <ListItem name="차종" title="{segment}" />
+      <ListItem name="차종" title="{segment}" />
+      <ListHeader title="월 비용" />
+      <ListItem name="차종" title="{segment}" />
+    </Wrapper>
+  );
 }
 
 export default DetailPage;
+
+const Wrapper = styled.div``;
