f17d483287d987b2070266d3d96648611808670f
so0112 <tkddh0112@gmail.com>
2022-11-03 20:33:29 +0900

    Style: GlobalStyle명 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { ThemeProvider } from 'styled-components';
-import All from './components/All';
-import GlobalStyles from './styles/GlobalStyles';
+import GlobalStyle from './styles/GlobalStyle';
 import { theme } from './styles/theme';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ListPage from './pages/ListPage';
@@ -9,12 +8,11 @@ import DetailPage from './pages/DetailPage';
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyles />
+      <GlobalStyle />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<All />} />
-          <Route path="/list" element={<ListPage />} />
-          <Route path="/detail" element={<DetailPage />} />
+          <Route path="/" element={<ListPage />} />
+          <Route path="/:id" element={<DetailPage />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
