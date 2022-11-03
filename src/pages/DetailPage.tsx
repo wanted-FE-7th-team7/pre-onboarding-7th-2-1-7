3145a9bff83e93b109dc9aaf354ac114fd976380
so0112 <tkddh0112@gmail.com>
2022-11-03 20:35:29 +0900

    Feat: List, Detail 페이지 추가

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -2,21 +2,72 @@ import React from 'react';
 import HearderBar from '../components/HeaderBar';
 import ListHeader from '../components/ListHeader';
 import ListItem from '../components/ListItem';
+import { Car } from '../models';
+import styled from 'styled-components';
+import { useLocation } from 'react-router';
+import { FUEL_TEXT, SEGMENT_TEXT } from '../utils';
+import { formatStartDate } from '../utils/format';
+
+const HEADER_INFO = '차량 정보';
+const HEADER_INSURANCE = '보험';
+const HEADER_ADDITIONAL_PRODUCT = '추가 상품';
+const NAME_SEGMENT = '차종';
+const NAME_FUEL = '연료';
+const NAME_START_DATE = '이용 가능일';
 
 function DetailPage() {
+  const { state: car } = useLocation();
+  const { amount, attribute, additionalProducts, insurance, startDate }: Car =
+    car;
+
   return (
-    <div>
+    <S.DetailPage>
       <HearderBar />
+      <img src={attribute.imageUrl} className="car-img" alt="img" />
       <div>
-        <img className="car-img" />
+        <h3 className="brand-name">{attribute.brand}</h3>
+        <h3 className="brand-name">{attribute.name}</h3>
       </div>
-      <div>
-        <div>brand</div>
-        <div>name</div>
-      </div>
-      <ListItem />
-      <ListHeader />
-    </div>
+      <ListItem name="" description={`월 ${amount} 원`} />
+      <ListHeader text={HEADER_INFO} />
+      <ListItem
+        name={NAME_SEGMENT}
+        description={SEGMENT_TEXT[attribute.segment]}
+      />
+      <ListItem name={NAME_FUEL} description={FUEL_TEXT[attribute.fuelType]} />
+      <ListItem
+        name={NAME_START_DATE}
+        description={formatStartDate(new Date(startDate))}
+      />
+      <ListHeader text={HEADER_INSURANCE} />
+      {insurance?.map(({ name, description }, idx) => (
+        <ListItem key={idx} name={name} description={description} />
+      ))}
+      <ListHeader text={HEADER_ADDITIONAL_PRODUCT} />
+      {additionalProducts?.map(({ name, amount }, idx) => (
+        <ListItem key={idx} name={name} description={`월 ${amount} 원`} />
+      ))}
+    </S.DetailPage>
   );
 }
+
+const S = {
+  DetailPage: styled.div`
+    display: flex;
+    flex-direction: column;
+
+    .car-img {
+      width: 33rem;
+      height: 20rem;
+      background-color: ${props => props.theme.gray};
+      margin-bottom: 2rem;
+    }
+
+    .brand-name {
+      margin-left: 0.5rem;
+      font-size: 2rem;
+      font-weight: 700;
+    }
+  `,
+};
 export default DetailPage;
--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -1,20 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import CarItem from '../components/CarItem';
 import Category from '../components/Category';
 import HearderBar from '../components/HeaderBar';
-
-const arr = [1, 2, 3, 4, 5];
+import getCars from '../apis/car';
+import styled from 'styled-components';
+import { Car, Segment } from '../models';
+import { useNavigate } from 'react-router-dom';
 
 function ListPage() {
+  const [cars, setCars] = useState<Car[]>([]);
+
+  useEffect(() => {
+    getCars(setCars);
+  }, []);
+
+  const navigate = useNavigate();
+
+  const handleClick = (id: number, carDetail: Car) => {
+    navigate(`/${id}`, { state: carDetail });
+  };
+
   return (
-    <div>
+    <S.ListPage>
       <HearderBar />
       <Category />
-      {arr.map(item => (
-        <CarItem key={item} />
+      {cars.map(car => (
+        <div
+          key={car.id}
+          onClick={() => {
+            handleClick(car.id, car);
+          }}
+        >
+          <CarItem
+            createdAt={car.startDate}
+            brand={car.attribute.brand}
+            name={car.attribute.name}
+            segment={car.attribute.segment}
+            fuelType={car.attribute.fuelType}
+            amount={car.amount}
+            imageSrc={car.attribute.imageUrl}
+          />
+        </div>
       ))}
-    </div>
+    </S.ListPage>
   );
 }
+
+const S = {
+  ListPage: styled.div``,
+};
 
 export default ListPage;
