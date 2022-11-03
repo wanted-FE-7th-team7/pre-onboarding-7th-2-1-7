a5fb07fe6f9563cfb47f6cafeafe894a320b20da
so0112 <tkddh0112@gmail.com>
2022-11-03 20:34:13 +0900

    Feat: CarItem 컴포넌트

--- src/components/CarItem.tsx
+++ src/components/CarItem.tsx
@@ -1,37 +1,58 @@
 import React from 'react';
 import styled from 'styled-components';
 
-function CarItem() {
+interface Props {
+  createdAt: Date;
+  brand: string;
+  name: string;
+  segment: string;
+  fuelType: string;
+  amount: number;
+  imageSrc: string;
+}
+
+function CarItem({
+  createdAt,
+  brand,
+  name,
+  segment,
+  fuelType,
+  amount,
+  imageSrc,
+}: Props) {
   return (
-    <SCarItem>
+    <S.CarItems>
       <div>
         <div className="brand-name">
-          <div>brand</div>
-          <div>name</div>
+          <div>{brand}</div>
+          <div>{name}</div>
         </div>
         <div className="type-amount">
-          <div>classType / fuelType</div>
-          <div>월 amount 원 부터</div>
+          <div>classType / {fuelType}</div>
+          <div>월 {amount} 원 부터</div>
         </div>
       </div>
-      <div className="car-img" />
-    </SCarItem>
+      <img src={imageSrc} className="car-img" />
+    </S.CarItems>
   );
 }
 
-const SCarItem = styled.div`
-  width: 30rem;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+const S = {
+  CarItems: styled.div`
+    width: 32rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 
-  padding: 0.5rem;
+    padding: 0.5rem;
+    border-bottom: 2px solid black;
 
-  .car-img {
-    width: 10rem;
-    height: 5rem;
+    .car-img {
+      width: 13rem;
+      height: 7rem;
 
-    background-color: ${props => props.theme.gray};
-  }
-`;
+      background-color: ${props => props.theme.gray};
+    }
+  `,
+};
 export default CarItem;
